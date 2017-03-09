63e18626953e331dc9ed7d6d5430d75eae54e287
Pedro José Anula <pedrojanula@gmail.com>
2017-03-09 01:28:40 +0100

    Added 116 - ¡Hola Mundo!

---
+++ 116 - ¡Hola Mundo!/HolaMundo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i, n;
+    
+    scanf("%i", &n);
+    for(i=0; i<n; i++){
+        printf("Hola mundo.\n");
+    }
+    
+    return EXIT_SUCCESS;
+}
+
